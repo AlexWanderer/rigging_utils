dee6de0eb607b69908411a1f4f5689fafb8b73b2
user <user@blendah.(none)>
2013-04-03 15:52:59 +0300

    Finished writing script, now testing

--- bone_colors.py
+++ bone_colors.py
@@ -16,103 +16,165 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 # 
-#  Authors           : Kfir Merlaub, Tamir Lousky
-#  Homepage(Wiki)    : http://kfirmerlaub.wix.com/     ( Kfir Merlaub (kfir.merlaub@gmail.com) )
-#  Homepage(Wiki)    : http://bioblog3d.wordpress.com/ ( Tamir Lousky (tlousky@gmail.com)      )
+#  Authors: 
+#       Tamir Lousky
+#           Homepage: http://bioblog3d.wordpress.com/
+#           Contact:  tlousky@gmail.com
+#       Kfir Merlaub
+#           Homepage: http://kfirmerlaub.wix.com/
+#           Contact:  kfir.merlaub@gmail.com
 #
-#  Start of project              : 2013-01-25 
-#  Last modified                 : 2013-03-20
+#  Start of project : 2013-01-25 
+#  Last modified    : 2013-04-03
 #
 #  Acknowledgements 
 #  ================
 #
-#  Blender: Nathan Vegdahl (who taught us everything we know about blender py-rigging) 
-#          
-
-
-""" TODO:
-1. UI:
-   checkbox   layer name    theme/color        [ loop this for all layers ]
-   Randomize [button] # apply random color for each of the active (checked) layers
-
-2. layer checkbox creates or deletes a bone group with a predefined name: "rig_utils_layer_(X)"
-
-Notes:
-armature layers in:
-bpy.data.armatures[idx].layers
-"""
-
+#  Blender: Nathan Vegdahl (who taught us everything we know about 
+#           blender py-rigging)
+ 
 bl_info = {    
     "name"       : "Armature Utils",
     "authors"    : [ "Kfir, Merlaub", "Tamir Lousky" ],
     "version"    : (1, 0, 0),
     "blender"    : (2, 66, 0),
-    "category"   : "Materials",
+    "category"   : "Rigging",
     "location"   : "Properties >> Armature",
     "wiki_url"   : "",
     "tracker_url": "",
-    "description": "Various rigging utilities."
+    "description": "Assign bone colors by the rig layers they are on."
 }
 
 import bpy
 
-class random_mat_panel(bpy.types.Panel):
-    bl_idname      = 'rigging_utils'
-    bl_label       = 'Rigging Utilities'
+def find_active_layers( obj ):
+    ''' This function iterates over all bones in the rig, and returns a list of
+         all the layers on which these bones are located '''
+
+    all_active_layers = []
+
+
+    for bone in obj.data.bones:
+        # Find bone's active layers. An active layer has a True value.
+        active_layers = [ l for l in bone.layers if l ]
+        
+        # Append new values to the list of all active layers
+        for l in active_layers:
+            if l not in all_active_layers:
+                active_bones.append( l )
+
+    return all_active_layers
+
+
+class bone_colors( bpy.types.Panel ):
+    bl_idname      = 'BoneColorsPanel'
+    bl_label       = 'Assign Bone Colors'
     bl_space_type  = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_context     = 'object'
+    bl_context     = 'posemode'
 
     @classmethod
     def poll(self, context):
+        # print( "Reached panel poll" )
+        # print( context.active_object.type )
         try:
             return ( context.active_object.type == 'ARMATURE' )
         except:
             return False
 
-    def draw( self, context):                # Draw panel UI elements #
-        layout = self.layout                 # Reference to panel layout object
+    def draw( self, context):
+        # Draw panel UI elements #
+        layout = self.layout               # Reference to panel layout object
+
+        obj         = bpy.context.object   # Ref to active armature
+        color_props = obj.bonegroup_colors # Ref to property group
+        bgroups     = obj.pose.bone_groups # Ref to bone groups
+
+        # Draw "use colors" checkbox
+        col        = layout.column()
+        col.prop( color_props.use_color, "Create Color Groups by Rig Layer" )
+
+        if color_props.use_color:
+            # each bone group gets a row where you can choose its color
+            for g in bgroups:
+                ''' I think this bit is unnecessary
+                col        = layout.column()
+                col.active = (obj.proxy is None)
+                col.prop(g, "name")
+                '''
+
+                split = layout.split()
+                split.active = (obj.proxy is None)
+
+                col = split.column()
+                col.prop(g, "color_set")  # Choose group color theme
+
+                if group.color_set:
+                    col = split.column()
+                    sub = col.row( align = True )
+                    sub.prop(g.colors, "normal", text="") # Theme normal color
+                    sub.prop(g.colors, "select", text="") # Theme select color
+                    sub.prop(g.colors, "active", text="") # Theme active color
         
-#        props = context.scene.face_assigner
 
-        col1 = layout.column()               # Create a column
-        col1.label(text="Randomly assign materials to each face on the object")
+class ColorBones( bpy.types.PropertyGroup ):
+
+    def create_groups( self ):
+        """ Creates bone groups by rig layers """
+        obj = bpy.context.object
+
+        # Exit and do not create groups if "use_colors" is set to False
+        if self.use_colors == False:
+            return None
         
-        box = layout.box()                   # Draw a box
-        col2 = box.column(align=True)        # Create a column
-        col2.prop( props, "rand_seed"  )     # Create randomization seed property on column
-        col2.label(text="use this field to filter materials by name")
-        col2.prop( props, "mat_prefix" )     # Material prefix property too
-        col2.label(text="Distribute materials according to:")
-        col2.prop( props, "assign_method" )  # Material assignment method prop
+        active_layers = find_active_layers( obj )
 
-
-def color_bones( obj ):
-
-    # Assign bone groups and colors
-    colors = {
-        'red'    : 'THEME01',
-        'blue'   : 'THEME04',
-        'green'  : 'THEME03',
-        'yellow' : 'THEME09',
-        'purple' : 'THEME06'
-    }
-
-    bpy.ops.object.mode_set(mode='POSE')
-
-    for c in colors:
-        bpy.ops.pose.group_add()
-        obj.pose.bone_groups[-1].name      = c
-        obj.pose.bone_groups[-1].color_set = colors[c]
-
-    for bone in obj.pose.bones:
-
-        # Set bone group by name
-        if   '.L' in bone.name:
-            bone.bone_group = obj.pose.bone_groups['red'   ]
-        elif '.R' in bone.name:
-            bone.bone_group = obj.pose.bone_groups['blue'  ]
-        else:
-            bone.bone_group = obj.pose.bone_groups['yellow']
+        # Get existing bone group names  
+        bgroups = [ g.name for g in obj.pose.bone_groups ]
         
-    bpy.ops.object.mode_set(mode='OBJECT') 
+        for l in active_layers:
+            
+            # Make bone Group name from bone layer number
+            bgroup_name = 'bgroup' + str(l)
+
+            # Create a new bone group if it doesn't exist
+            if bgroup_name not in bgroups:
+                bpy.ops.pose.group_add()
+                obj.pose.bone_groups[-1].name = bgroup_name
+                
+            ## Add bones to this group (if they're on the appropriate layer )
+            
+            # Reference all the bones on this layer
+            layer_bones = [ b.name for b in obj.data.bones if b.layers[l] ]
+
+            """ Note that this addon will not work as expected if bones are on
+                more than one layer. The group representing the highest number
+                layer will contain a bone with more than one layer.
+            """
+
+            # Add bones to group
+            for bone in layer_bones:
+                pb = obj.pose.bones[ bone ]
+                pb.bone_group = obj.pose.bone_groups[ bgroup_name ]
+            
+        return None
+
+    use_colors = bpy.props.BoolProperty(
+        name        = "use_colors",
+        description = "Color bones by rig layers",
+        default     = False,
+        update      = create_groups
+    )  
+
+
+def register():
+    bpy.utils.register_module(__name__)
+    bpy.utils.register_class( bone_colors )
+
+    # Ref to prop group via dynamic object property
+    bpy.types.Object.bonegroup_colors = \
+        bpy.props.PointerProperty( type = ColorBones )
+    
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    bpy.utils.unregister_class( bone_colors )
